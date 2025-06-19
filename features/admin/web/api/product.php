2c892f2c8dc4bdfeda45af12f05ac69ea2cc3750
Miles <MilesBriones@cvsu.edu.ph>
2025-06-19 15:18:22 +0800

    a

--- features/admin/web/api/product.php
+++ features/admin/web/api/product.php
@@ -15,10 +15,10 @@ if (!isset($_SESSION['email']) || !isset($_SESSION['role']) || $_SESSION['role']
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product | Admin</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../css/category-list.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
       <link rel="icon" href="../../../../assets/img/logo.png" type="image/x-icon">
 
 </head>
@@ -661,7 +661,7 @@ if (!isset($_SESSION['email']) || !isset($_SESSION['role']) || $_SESSION['role']
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" crossorigin="anonymous">
 </script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </script>
 <script src="../../function/script/toggle-menu.js"></script>
 <script src="../../function/script/pagination.js"></script>

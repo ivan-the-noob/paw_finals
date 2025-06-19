ec7441b9632e45e36850fcaed55f51b957686bb7
Miles <MilesBriones@cvsu.edu.ph>
2025-06-19 15:19:16 +0800

    a

--- features/admin/web/api/product.php
+++ features/admin/web/api/product.php
@@ -18,6 +18,8 @@ if (!isset($_SESSION['email']) || !isset($_SESSION['role']) || $_SESSION['role']
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../css/category-list.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+
 
       <link rel="icon" href="../../../../assets/img/logo.png" type="image/x-icon">
 
